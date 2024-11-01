b4785bdef4e005d86cc94b1d3f0c74881cabb73e
Edwxn <edwinpena0306@gmail.com>
2024-11-01 16:41:55 -0400

    creación de función

---
+++ script/main.js
@@ -0,0 +1,26 @@
+const PasswordBoxEl = document.getElementById("password-box");
+const length = 10;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "1234567890";
+const symbols = "._-";
+
+const allChars = upperCase + lowerCase + numbers + symbols;
+
+function createRandomPassword() {
+    let password = "";
+
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += numbers[Math.floor(Math.random() * numbers.length)];
+    password += symbols[Math.floor(Math.random() * symbols.length)];
+
+    while (length > password.length) {
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+
+    password = password.split('').sort(() => Math.random() - 0.5).join('');
+
+    PasswordBoxEl.value = password;
+}
